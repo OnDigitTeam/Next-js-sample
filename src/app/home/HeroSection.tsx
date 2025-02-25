c2bb3a2b8e1ed45670f2c89de5926623661435f0
OnDigitTeam <team@ondigit.com>
2025-02-25 12:50:00 +0530

    Update HeroSection.tsx

--- src/app/home/HeroSection.tsx
+++ src/app/home/HeroSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function HeroSection() {
     return (
         <div>
-            <div className="container w-full flex flex-1 justify-center">
+            <div className="container w-full flex flex-1 justify-center mx-auto">
 
                 <Image
 
